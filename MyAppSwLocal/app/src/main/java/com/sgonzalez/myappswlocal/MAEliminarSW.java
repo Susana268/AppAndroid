2899901fbb5d45d45965008aaf139c4b39f2bcb3
unknown <balvygonzalez94@gmail.com>
2022-10-10 21:42:47 -0600

    10/12/2022 Tarea Pinturas

---
+++ MyAppSwLocal/app/src/main/java/com/sgonzalez/myappswlocal/MAEliminarSW.java
@@ -0,0 +1,14 @@
+package com.sgonzalez.myappswlocal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MAEliminarSW extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maeliminar_sw);
+    }
+}
---
+++ MyAppSwLocal/app/src/main/java/com/sgonzalez/myappswlocal/MAListarMostrarSW.java
@@ -0,0 +1,14 @@
+package com.sgonzalez.myappswlocal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MAListarMostrarSW extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_malistar_mostrar_sw);
+    }
+}
