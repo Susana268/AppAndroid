4f0ede3113ef4d736686cb823ef552c933009e67
unknown <balvygonzalez94@gmail.com>
2022-10-10 21:50:08 -0600

    10/10/2022 Tarea Pinturas

--- MyAppPinturas/app/src/main/java/com/sgonzalez/myapppinturas/MainCompra.java
+++ MyAppPinturas/app/src/main/java/com/sgonzalez/myapppinturas/MainCompra.java
@@ -2,12 +2,16 @@ package com.sgonzalez.myapppinturas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainCompra extends AppCompatActivity {
-    private TextView txtViewNom, txtViewPrecio;
+    private TextView txtViewNom, txtViewPrecio,txtViewNomCte,txtViewApeCte,txtViewNitCte;
     private ImageView imgView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +20,35 @@ public class MainCompra extends AppCompatActivity {
         txtViewNom = findViewById(R.id.txtNomCompra);
         txtViewPrecio = findViewById(R.id.txtPrecioCompra);
         imgView = findViewById(R.id.imgCompra);
+        txtViewNomCte = findViewById(R.id.txtNomCteCompra);
+        txtViewApeCte = findViewById(R.id.txtApeCteCompra);
+        txtViewNitCte = findViewById(R.id.txtNitCteCompra);
         this.obtener();
+        Button button = findViewById(R.id.btnComprar);
+        Button buttonCancel = findViewById(R.id.btnCancelar);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nomPinturaDato = txtViewNom.getText().toString();
+                int precioPinturaDato = Integer.parseInt(txtViewPrecio.getText().toString());
+                String nomCtePinturaDato = txtViewNomCte.getText().toString();
+                String apeCtePinturaDato = txtViewApeCte.getText().toString();
+                String nitCtePinturaDato = txtViewNitCte.getText().toString();
+                trasladoAdapter(nomPinturaDato,precioPinturaDato,nomCtePinturaDato,
+                                apeCtePinturaDato,nitCtePinturaDato);
+            }
+        });
+
+        buttonCancel.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainCompra.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 
     private void obtener() {
@@ -29,5 +61,18 @@ public class MainCompra extends AppCompatActivity {
         txtViewNom.setText(String.valueOf(nombre));
         txtViewPrecio.setText(String.valueOf(precio));
 
+    }
+    private void trasladoAdapter(String nombre, int precio,String nombreCte,
+                                 String apellidoCte, String nitCte){
+        Intent intent = new Intent(this,MainConfirmaCompra.class);
+
+        intent.putExtra("nombre",nombre);
+        intent.putExtra("precio",precio);
+        intent.putExtra("nomcte",nombreCte);
+        intent.putExtra("apecte",apellidoCte);
+        intent.putExtra("nitCte",nitCte);
+
+        this.startActivity(intent);
+        finish();
     }
 }
--- MyAppPinturas/app/src/main/java/com/sgonzalez/myapppinturas/MainConfirmaCompra.java
+++ MyAppPinturas/app/src/main/java/com/sgonzalez/myapppinturas/MainConfirmaCompra.java
@@ -2,13 +2,53 @@ package com.sgonzalez.myapppinturas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainConfirmaCompra extends AppCompatActivity {
-
+    private TextView txtViewNomF, txtViewPrecioF,txtViewNomCteF,txtViewApeCteF,txtViewNitF;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_confirma_compra);
+        txtViewNomF = findViewById(R.id.txtNomCompraF);
+        txtViewPrecioF = findViewById(R.id.txtPrecioCompraF);
+        txtViewNomCteF = findViewById(R.id.txtNomCteF);
+        txtViewApeCteF = findViewById(R.id.txtApeCteF);
+        txtViewNitF = findViewById(R.id.txtNitCteF);
+        this.obtener();
+        Button button = findViewById(R.id.btnFinal);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(MainConfirmaCompra.this,MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
     }
+    private void obtener() {
+        Bundle bundle = getIntent().getExtras();
+        String nombre = bundle.getString("nombre");
+        Integer precio = bundle.getInt("precio");
+        String nomcte = bundle.getString("nomcte");
+        String apecte = bundle.getString("apecte");
+        String nitCte = bundle.getString("nitCte");
+
+        //-----
+        txtViewNomF.setText(String.valueOf(nombre));
+        txtViewPrecioF.setText(String.valueOf(precio));
+        txtViewNomCteF.setText(String.valueOf(nomcte));
+        txtViewApeCteF.setText(String.valueOf(apecte));
+        txtViewNitF.setText(String.valueOf(nitCte));
+
+    }
+
 }
